7239efe7e1c82013ca7c020d34e6f9c91aa87cf6
Martin Vium <martin.vium@gmail.com>
2010-02-19 18:41:41 +0100

    Implemented tests for Iterator rule

--- tests/VXML/Rule/IteratorTest.php
+++ tests/VXML/Rule/IteratorTest.php
@@ -7,26 +7,47 @@ class VXML_Rule_IteratorTest extends VXML_Rule_TestCase
 {
 	public function testIteratableTarget()
 	{
-		$this->markTestIncomplete();
+		$rule = new Rule\Iterator(new Rule\Regex('friends', array('pattern' => '/@/')));
+		$this->assertTrue($rule->execute($this->context, $this->response));
 	}
 	
+	public function testIterableTargetsFailure()
+	{
+		$rule = new Rule\Iterator(new Rule\Regex('blocked', array('pattern' => '/@/')));
+		$this->assertFalse($rule->execute($this->context, $this->response));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
 	public function testSingleTarget()
 	{
-		$this->markTestIncomplete();
+		$rule = new Rule\Iterator(new Rule\Regex('email', array('pattern' => '/@/')));
+		$rule->execute($this->context, $this->response);
 	}
 	
-	public function testMinimumOption()
+	public function testMinimumOptionSuccess()
 	{
-		$this->markTestIncomplete();
+		$rule = new Rule\Iterator(new Rule\Regex('blocked', array('pattern' => '/@/')), array('min' => 1));
+		$this->assertTrue($rule->execute($this->context, $this->response));
 	}
 	
-	public function testMaximumOption()
+	public function testMinimumOptionTooFew()
 	{
-		$this->markTestIncomplete();
+		$rule = new Rule\Iterator(new Rule\Regex('blocked', array('pattern' => '/@/')), array('min' => 2));
+		$this->assertFalse($rule->execute($this->context, $this->response));
 	}
 	
-	public function testMissingPathOption()
+	public function testMaximumOptionSuccess()
 	{
-		$this->markTestIncomplete();
+		$rule = new Rule\Iterator(new Rule\Regex('blocked', array('pattern' => '/@/')), array('min' => 1, 'max' => 1));
+//		$this->assertEquals(array(), array_map(function ($msg) {return $msg['type']; }, $this->response->getAllMessages()));
+		$this->assertTrue($rule->execute($this->context, $this->response));
+	}
+	
+	public function testMaximumOptionFailure()
+	{
+		$rule = new Rule\Iterator(new Rule\Regex('friends', array('pattern' => '/@/')), array('max' => 1));
+		$this->assertFalse($rule->execute($this->context, $this->response));
 	}
 }
--- tests/VXML/Rule/TestCase.php
+++ tests/VXML/Rule/TestCase.php
@@ -23,11 +23,18 @@ abstract class VXML_Rule_TestCase extends PHPUnit_Framework_TestCase
 		$this->mockData = array(
 			'firstname' 		=> 'Jørgen',
 			'lastname'			=> 'Andersen',
+			'email'				=> 'jorgen@example.com',
 			'salary'			=> 25000,
 			'empty'				=> '',
 			'birthdate_year' 	=> 1984,
 			'birthdate_month'	=> 05,
-			'birthdate_day'		=> 23
+			'birthdate_day'		=> 23,
+			'friends'			=> array(
+				'mock-a@example.com',
+				'mock-b@example.dk'),
+			'blocked'			=> array(
+				'mock-cexample.net',
+				'mock-d@example.se')
 		);
 		
 		$this->context = new VXML\Context($this->mockData);
