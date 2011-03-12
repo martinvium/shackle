b8a0b62b05bb942f4cd0eda99d7cd17dd5e6c7ce
Martin Vium <martin.vium@gmail.com>
2011-03-12 12:47:48 +0100

    Fix double library in include path instead of tests folder

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 $vxml_library = dirname(__DIR__) . '/library';
 $vxml_tests = __DIR__;
-set_include_path(get_include_path() . PATH_SEPARATOR . $vxml_library . PATH_SEPARATOR . $vxml_library);
+set_include_path(get_include_path() . PATH_SEPARATOR . $vxml_library . PATH_SEPARATOR . $vxml_tests);
 
 require_once 'VXML/Loader.php';
 VXML\Loader::registerAutoload();
